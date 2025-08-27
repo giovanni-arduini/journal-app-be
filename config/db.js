1bea6769a5505350710e8243d59d973fcd55f209
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-27 16:51:48 +0200

    ADD db connection

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/experiences");
+    console.log("✅ MongoDB connesso");
+  } catch (err) {
+    console.error("❌ Errore connessione MongoDB:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
